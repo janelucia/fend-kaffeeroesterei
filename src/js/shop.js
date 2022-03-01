935269092b6b4fb25ef88bfcefec76208fccbaef
Jane <janeluciaschoenfeld@gmail.com>
2022-03-01 19:37:35 +0100

    handlebar improvement and shop js

--- src/js/shop.js
+++ src/js/shop.js
@@ -3,20 +3,20 @@ import products from "./products.json";
 function createProductElements() {
   const productSection = document.querySelector(".shop-products-section");
 
-  products.forEach((product) => {
+  products.products.forEach((product) => {
     const productElement = document.createElement("div");
     productElement.classList.add("product");
 
     const productImageWrapper = document.createElement("div");
-    productImageWrapper.classList.add('product-img-wrapper');
+    productImageWrapper.classList.add("product-img-wrapper");
 
     const productImage = document.createElement("img");
     productImage.classList.add("product-img");
     productImage.setAttribute("src", product.productImage);
-    productImage.setAttribute( "alt", product.altText);
+    productImage.setAttribute("alt", product.altText);
 
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
 
     const productName = document.createElement("h4");
     productName.classList.add("product-name");
@@ -28,18 +28,33 @@ function createProductElements() {
     const productPriceText = document.createTextNode(product.price);
     productPrice.appendChild(productPriceText);
 
+    const productIconWrapper = document.createElement("div");
+    productIconWrapper.classList.add("product-icon-wrapper");
+
+    const icon = (iconPath) => {
+      const icon = document.createElement("img");
+      icon.classList.add("product-icon");
+      icon.setAttribute("src", iconPath);
+      productIconWrapper.appendChild(icon);
+    };
+
     productImageWrapper.appendChild(productImage);
+
+    if (product.icons) {
+      product.icons.forEach((iconName) => {
+        icon(products.commons.iconPaths[iconName]);
+      })
+    } 
 
     productInfo.appendChild(productName);
     productInfo.appendChild(productPrice);
+    productInfo.appendChild(productIconWrapper);
 
     productElement.appendChild(productImageWrapper);
     productElement.appendChild(productInfo);
 
     productSection.appendChild(productElement);
   });
-
-  // productSection.appendChild(productContainer);
 }
 
 const shop = () => {
